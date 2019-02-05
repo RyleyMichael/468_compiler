f2edb59a6fd20d855a7f5cf9561ecd47b191e31e
Gio <giovany.m.addnu@gmail.com>
2019-02-05 11:03:05 -0700

    Second commit

--- gen/Driver.java
+++ gen/Driver.java
@@ -17,7 +17,8 @@ public class TerminalListener extends MicroBaseListener {
         tokens.add(node.getSymbol().getText());
 
     }
-
+    // This Method writes the treminals the listener havs visited to an output
+    //
     public void output_terminals(){
         File output = new File
         for(String vals:values){
@@ -31,16 +32,24 @@ public class TerminalListener extends MicroBaseListener {
 public static void main(String[] args){
 
         for(String s:args){
-            File input = new File(s)
+            //Open input files
+            File input = new File(s);
+            //Convert the input file to string
             Scanner s = new Scanner(input).useDelimiter("\\A");
             String result = s.hasNext()?s.next():"";
+            //Apply Lexer (Tokenizer) to input string
             MicroLexer lexer = new Microlexer(CharStreams.fromString(result));
             CommonTokenStream tokens = new CommonTokenStream(lexer);
+            //Create a Parser
             MicroParser parser = new MicroParser(tokens);
             ParseTree tree = parser.compilationUnit();
+            //Craete a walker object to traverse the arse tree
             ParseTreeWalker walker = new ParseTreeWalker();
+            //Craete a treminal listner to collect the non-treminals in the niput file
             TerminalListener listener = new TerminalListener();
+            //Traverse the tree
             walker.walk(listener, tree);
+            //Craete output file for terminals
             listener.output_terminals();
             input.close();
         }
