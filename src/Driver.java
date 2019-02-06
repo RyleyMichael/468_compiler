215b46dd76e7bffa8d3237f9b367cce6b73dafef
Gio <giovany.m.addnu@gmail.com>
2019-02-06 15:09:58 -0700

    Second commit

--- src/Driver.java
+++ src/Driver.java
@@ -2,6 +2,7 @@ import org.antlr.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.misc.NotNull;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
+import org.antlr.v4.runtime.tree.TerminalNodeImpl;
 
 import java.util.*;
 import java.io.*;
@@ -11,30 +12,32 @@ import java.lang.String;
 class Driver{
     public static void main(String[]args){
         try {
-            //for (String s : args) {
-                //Open input files
-                File input = new File("./src/nested.micro");
+            for (String s : args) {
+                String filename = "./src/nested.micro";
+                File input = new File(filename);
                 //Convert the input file to string
                 Scanner scanner = new Scanner(input).useDelimiter("\\A");
                 String result = scanner.hasNext() ? scanner.next() : "";
                 //Apply Lexer (Tokenizer) to input string
                 MicroLexer lexer = new MicroLexer(CharStreams.fromString(result));
+                Vocabulary v = lexer.getVocabulary();
                 CommonTokenStream tokens = new CommonTokenStream(lexer);
                 //Create a Parser
                 MicroParser parser = new MicroParser(tokens);
                 RuleContext tree = parser.program();
-                // Create a walker object to traverse the arse tree
+            // Create a walker object to traverse the arse tree
                 ParseTreeWalker walker = new ParseTreeWalker();
-                //Create a treminal listener to collect the non-terminals in the niput file
-                TerminalListener listener = new TerminalListener();
+                //Create a treminal listener to collect the non-terminals in the input file
+                TerminalListener listener = new TerminalListener(v);
                 //Traverse the tree
                 walker.walk(listener, tree);
-                //Create output file for terminals
-                listener.output_terminals("name");
-            //}
+
+            //Create output file for terminals
+                listener.output_terminals(filename);
+            }
         }
         catch(Exception e){
-            System.out.println("\n Ab error occured" + e.getMessage());
+            System.out.println("\n Ab error occured\n" + e.getMessage() + "\n" +e.fillInStackTrace());
         }
     }
 }
--- src/TerminalListener.java
+++ src/TerminalListener.java
@@ -1,5 +1,10 @@
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.LinkedList;
 import java.util.Queue;
+
+import org.antlr.v4.runtime.Vocabulary;
 import org.antlr.v4.runtime.tree.*;
 
 //Terminal Listener class ovrerides the base lsitener class
@@ -7,24 +12,34 @@ import org.antlr.v4.runtime.tree.*;
 // We have ovreridden the "On visiting treminal" so that the class
 // maintains a list of the non-termnial tokens it's seen and their values.
 public class TerminalListener extends MicroBaseListener {
-    private Queue<String> tokens;
-    private Queue<String> values;
+    private Queue<String> tokens = new LinkedList<String>();
+    private Queue<String> values = new LinkedList<String>();
+    private Vocabulary v;
 
+    public TerminalListener(Vocabulary v){
+        this.v = v;
+    }
     @Override
     public void visitTerminal(TerminalNode node){
+
         values.add(node.getText());
-        tokens.add(node.getSymbol().getText());
+        tokens.add( " " + v.getSymbolicName(node.getSymbol().getType()));
+
 
     }
     // This Method writes the terminals the listener has visited to an output
     //
-    public void output_terminals(String filename){
-        //File output = new File(filename);
+    public void output_terminals(String filename) throws IOException {
+        int length = filename.length()-5;
+        String output_flie = (filename.substring(0,length))+"out";
+        FileWriter fileWriter = new FileWriter(output_flie);
         for(String vals:values){
             String toks = tokens.remove();
-            System.out.println("\n Tokens = " + toks + "\n Vals = " + vals);
+            fileWriter.write("\n Tokens = " + toks + "\n Vals = " + vals);
 
         }
+        fileWriter.close();
+
     }
 
 }
